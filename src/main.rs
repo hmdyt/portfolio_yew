6c79db80ed8e9964a2838af458a0acaa92576b8e
hmdyt <konnyaku108@gmail.com>
2022-02-16 21:43:09 +0900

    add test text

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ use yew::prelude::*;
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <h1>{ "Hello Worlda" }</h1>
+        <>
+            <h1>{ "Hello Worlda" }</h1>
+            <p> {"test"} </p>
+        </>
     }
 }
 
