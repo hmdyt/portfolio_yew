1349619b574944010a42c6adc838c6e25402c245
hmdyt <konnyaku108@gmail.com>
2022-02-17 22:37:15 +0900

    add toggle button

--- src/components/header.rs
+++ src/components/header.rs
@@ -1,22 +1,42 @@
 use yew::prelude::*;
 
-#[derive(Properties, PartialEq)]
+#[derive(Debug)]
+struct HeaderButton {
+    is_selected: bool,
+}
+
+#[derive(Properties, PartialEq, Debug)]
 struct HeaderButtonProps {
-    id: usize,
     is_selected: bool,
     text: String,
 }
 
-#[function_component(HeaderButton)]
-fn header_button(
-    HeaderButtonProps {
-        id,
-        is_selected,
-        text,
-    }: &HeaderButtonProps,
-) -> Html {
-    html! {
-        <a href="#">{text}</a>
+enum Msg {
+    Click,
+}
+
+impl Component for HeaderButton {
+    type Message = Msg;
+    type Properties = HeaderButtonProps;
+
+    fn create(ctx: &Context<Self>) -> Self {
+        Self {
+            is_selected: ctx.props().is_selected,
+        }
+    }
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::Click => {
+                self.is_selected = !self.is_selected;
+            }
+        };
+        true
+    }
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let click_callback = ctx.link().callback(|_| Msg::Click);
+        html! {
+            <a href="#" onclick={click_callback}>{format!("{} {}", ctx.props().text, self.is_selected)}</a>
+        }
     }
 }
 
@@ -25,8 +45,8 @@ pub fn header() -> Html {
     html! {
     <nav class="menu">
         <div class="inner">
-            <HeaderButton id={0} is_selected={false} text={"About"} />
-            <HeaderButton id={0} is_selected={false} text={"Works"} />
+            <HeaderButton is_selected={false} text={"About"} />
+            <HeaderButton is_selected={false} text={"Works"} />
         </div>
     </nav>
     }
